dfd0032b3d263e2390341f3f477fbf24f3d51571
Helmi-Simplon <shan.tunisois@gmail.com>
2021-05-27 22:32:56 +0200

    create files : template.php and footer.php + update index.php

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- meta référencement -->
-    <meta name="description" content="Web Dev PHP : Conditions, requêtes GET">
-    <meta name="author" content="Camile Ghastine">
-
-    <title>Questionnaire de satisfaction du service client Amazin</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://bootswatch.com/4/lumen/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-</head>
-
-<body>
-
-    <div class="container">
-
-        <h1 class="mb-5">AMAZIN</h1>
-
-        <h2>Questionnaire de satisfaction</h2>
         <?php
+        include("./view/template.php");
         if ($_GET == null) {
             include("./view/nullStep.php");
         } elseif (isset($_GET["step"]) && $_GET["step"] == 1) {
@@ -45,10 +23,5 @@
             $btn = isset($_GET['btn']) ? $_GET['btn'] : NULL;
             include("./view/fifthStep.php");
         }
+        include("./view/footer.php")
         ?>
-
-    </div>
-
-</body>
-
-</html>
