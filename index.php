9b04290baabf267171021aeaff2e2758da5de432
Helmi-Simplon <shan.tunisois@gmail.com>
2021-05-28 08:23:50 +0200

    update style index.php

--- index.php
+++ index.php
@@ -15,18 +15,19 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 
-<body>
+<body style="font-family: Roboto;font-size:larger;color:gray">
 
     <div class="container">
 
-        <h1 class="mb-5">AMAZIN</h1>
+        <h1 class="mb-5" style="font-weight:bold;background:black;color:whitesmoke;width:fit-content;box-shadow:8px 8px 0px gray;">AMAZIN</h1>
 
-        <h2>Questionnaire de satisfaction</h2>
+        <h2 style="background:white;color:gray;width:fit-content;box-shadow:8px 8px 0px gray;">Questionnaire de satisfaction</h2><br><br>
+        
         <?php
         include("./router/index.php");
         ?>
     </div>
-
+    
 </body>
 
 </html>
